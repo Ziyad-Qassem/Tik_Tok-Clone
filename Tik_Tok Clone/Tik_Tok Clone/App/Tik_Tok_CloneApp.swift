76baf2ee918673ecc69bb60846e9a12c00b725a9
Ziyad-Qassem <Qassemziyad007@gmail.com>
2024-08-12 03:12:20 +0300

    adding the project

---
+++ Tik_Tok Clone/Tik_Tok Clone/App/Tik_Tok_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tik_Tok_CloneApp.swift
+//  Tik_Tok Clone
+//
+//  Created by Ziyad Qassem on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tik_Tok_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
